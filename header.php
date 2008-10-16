cd65cb952d7827415c08e8e58ef11945ab321f29
Josh Wood <josh@impala.intranet.utopian.net>
2008-10-15 19:03:04 -0700

    Replace deprecated get_settings() with bloginfo().

--- header.php
+++ header.php
@@ -2,8 +2,8 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 
 <head profile="http://gmpg.org/xfn/11">
-	<title><?php if(is_404()): ?>Error 404 - Not Found - <?php elseif(is_search()): ?><?php the_search_query() ?> - <?php else: ?><?php wp_title('-',true,'right'); ?><?php endif ?><?php bloginfo('name') ?><?php if(is_search()): ?> Search<?php endif ?></title>
 	<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+	<title><?php if(is_404()): ?>Error 404 - Not Found - <?php elseif(is_search()): ?><?php the_search_query() ?> - <?php else: ?><?php wp_title('-',true,'right'); ?><?php endif ?><?php bloginfo('name') ?><?php if(is_search()): ?> Search<?php endif ?></title>
 	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="all" title="Simplish" />
 	<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS Feed" href="<?php bloginfo('rss2_url'); ?>" />
 	<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> comments RSS Feed" href="<?php bloginfo('comments_rss2_url'); ?>" />
@@ -17,7 +17,7 @@
 <body>
 <div id="container">
 	<div id="header">
-		<h1><span><a href="<?php echo get_settings('home'); ?>" rel="home"><?php bloginfo('name'); ?></a></span></h1>
+		<h1><span><a href="<?php bloginfo('url'); ?>" rel="home"><?php bloginfo('name'); ?></a></span></h1>
 		<h2><?php bloginfo('description'); ?></h2>
 	</div>
 	<div id="page">
