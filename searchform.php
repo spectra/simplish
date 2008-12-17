0cb0522cf913654390e291e3c773584a4d541cf7
Josh Wood <josh@impala.intranet.utopian.net>
2008-12-16 18:11:42 -0800

    use new get_search_form()

--- searchform.php
+++
@@ -1,6 +0,0 @@
-<form id="searchform" method="get" action="<?php bloginfo('home') ?>">
-	<div>
-		<input id="s" name="s" type="text" value="<?php the_search_query() ?>" />
-		<input id="searchsubmit" name="searchsubmit" type="submit" value="Search" />
-	</div>
-</form>
