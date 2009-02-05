895022ad2b561301e425162f11293f06233a1f71
Josh Wood <josh@impala.intranet.utopian.net>
2009-02-05 15:41:00 -0800

    switch to get_search_form() in last couple places

--- index.php
+++ index.php
@@ -24,8 +24,7 @@
 <?php else : ?>
 
 			<h2 class="center">Not Found</h2>
-			<p class="center">Sorry, but you are looking for something that isn't here.</p>
-			<?php include(TEMPLATEPATH . "/searchform.php"); ?>
+			<?php get_search_form(); ?>
 
 <?php endif; ?>
 
