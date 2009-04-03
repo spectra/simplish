059046b854e1f891150c54f539cc95735cf8716f
Josh Wood <josh@impala.intranet.utopian.net>
2009-04-02 21:19:26 -0700

    make Tags a localizable string by adjusting fucntion args to the_tags()

--- hentrymeta.php
+++ hentrymeta.php
@@ -3,7 +3,7 @@
 					<li class="categories"><?php _e('Category:', 'simplish') ?> <?php the_category(', ') ?></li>
 <?php endif; ?>
 <?php if(get_the_tags() != ''): ?>
-					<li class="tags"><?php the_tags(); ?></li>
+					<li class="tags"><?php the_tags(__('Tags: ', 'simplish')); ?></li>
 <?php endif; ?>
 					<li><a href="<?php comments_link(); ?>-heading"><?php comments_number(__('0 Comments', 'simplish'), __('1 Comment', 'simplish'), __('% Comments', 'simplish')); ?></a> - <?php comments_rss_link(__('Feed', 'simplish')); ?></li>
 				</ul>
