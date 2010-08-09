5c42948bd4d511aeab1ae63259194511dd1be010
utopian labs <utopian.labs@gmail.com>
2010-08-09 13:55:43 -0700

    Initial custom excerpt handling, as required. Borrows heavily from 2010 theme.

--- functions.php
+++ functions.php
@@ -38,6 +38,67 @@ function sp_widgets_init()
 }
 add_action('widgets_init', 'sp_widgets_init');
 
+/**
+ * Set post excerpt length.
+ *
+ * To override this in a child theme, remove the filter and add your own
+ * function tied to the excerpt_length filter hook.
+ * 
+ * All excerpt handling borrows heavily from the 2010 theme.
+ *
+ * @since Simplish 2.4.2 (Twenty Ten 1.0)
+ * @return int
+ */
+function sp_excerpt_length($length)
+{
+	return 36;
+}
+add_filter('excerpt_length', 'sp_excerpt_length');
+
+/**
+ * "More" link for excerpts
+ *
+ * @since Simplish 2.4.2 (Twenty Ten 1.0)
+ * @return string "More ->" link
+ */	
+function sp_readmore_link()
+{
+	return ' <a href="'. get_permalink() . '">' . __( 'More <span class="meta-nav">&rarr;</span>', 'simplish' ) . '</a>';
+}
+
+/**
+ * Replaces "[...]" (appended to auto-generated excerpts) with an ellipsis and sp_readmore_link().
+ *
+ * To override this in a child theme, remove the filter and add your own
+ * function tied to the excerpt_more filter hook.
+ *
+ * @since Simplish 2.4.2 (Twenty Ten 1.0)
+ * @return string An ellipsis and a link
+ */	
+function sp_auto_excerpt_more($more)
+{
+	return ' &hellip;' . sp_readmore_link();
+}
+add_filter('excerpt_more', 'sp_auto_excerpt_more');
+
+/**
+ * Add "More" link to custom post excerpts.
+ *
+ * To override this link in a child theme, remove the filter and add your own
+ * function tied to the get_the_excerpt filter hook.
+ *
+ * @since Simplish 2.4.2 (Twenty Ten 1.0)
+ * @return string The user-specified (not auto-generated) excerpt with a "More" link
+ */
+function sp_custom_excerpt_more($output)
+{
+	if ( has_excerpt() && ! is_attachment() ){
+		$output .= sp_readmore_link();
+	}
+	return $output;
+}
+add_filter( 'get_the_excerpt', 'sp_custom_excerpt_more' );
+
 /*
  * Zero the default gallery shortcode style, so it isn't injected into output body.
  * Simplish gallery styles are in style.css.
