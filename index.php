87b948dc401d8bab186db41dad618448b2ece7b0
Josh Wood <josh@utopian.net>
2011-01-28 16:37:58 -0800

    Revert post format changes for now. They are in the utopianlabs-postfmt clone.

--- index.php
+++ index.php
@@ -4,9 +4,18 @@
 
 <?php if(have_posts()): ?>
 
-<?php while(have_posts()): the_post();
-		get_template_part('format', get_post_format());
-endwhile; ?>
+<?php while(have_posts()): the_post(); ?>
+			<div id="article-<?php the_ID(); ?>" <?php post_class(); ?>>
+<?php get_template_part('hentryhead') ?>
+				<br class="clear" />
+				<div class="entry-content">
+					<?php the_content(sp_readmore_text()); ?>
+					<?php wp_link_pages( array( 'before' => '<div class="pgnum-link">' . __( 'Pages:', 'simplish' ), 'after' => '</div>' ) ); ?>
+				</div>
+<?php get_template_part('hentrymeta') ?>
+			</div><!--#article-num .hentry-->
+
+<?php endwhile; ?>
 
 <?php get_template_part('prevnextnav') ?>
 
